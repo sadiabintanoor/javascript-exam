cb886d9c4d38cd6a5dbb5b9b14b62ae6f777106b
sadiabintanoor <sadiabintenoor4@gmail.com>
2024-09-27 19:39:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function myname(){
+    document.getElementById("myname").innerHTML ="WEB DEVELOPER"
+}
+function myimage() {
+   document.getElementById('myimage').src='image/kathgolap.jpg'
+  }
+
+function image(){
+    document.getElementById('myimage').src='image/adenium.jpg'
+}
+
+function fontstyle(){
+   document.getElementById('fontstyle').style.fontSize="50px";
+}
+
+function hidetext(){
+    document.getElementById('hide').style.display='none'
+}
+function blockelement(){
+    document.getElementById('block-element').style.display='block'
+}
